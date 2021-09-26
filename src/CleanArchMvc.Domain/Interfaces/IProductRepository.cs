7cf59d4b506ed70d9909a8d1c16cb5a5baf1c78b
joaomlg <joaomarcos1999jm@gmail.com>
2021-09-26 12:31:48 -0300

    Define repositories interfaces

---
+++ src/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+
+namespace CleanArchMvc.Domain.Interface
+{
+  public interface IProductRepository
+  {
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetByIdAsync(int? id);
+    Task<IEnumerable<Product>> GetProductsByCategoryIdAsync(int? id);
+    Task<Product> CreateAsync(Product product);
+    Task<Product> UpdateAsync(Product product);
+    Task<Product> RemoveAsync(Product product);
+  }
+}
