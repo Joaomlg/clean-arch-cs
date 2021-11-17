6200096751e65656b6a448f17710c17b46397776
joaomlg <joaomarcos1999jm@gmail.com>
2021-11-16 21:42:17 -0300

    Products controller methods and views implemented

--- src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -10,16 +13,110 @@ namespace CleanArchMvc.WebUI.Controllers
     public class ProductsController : Controller
     {
         private IProductService _productService;
+        private ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var products = await _productService.GetProductsAsync();
             return View(products);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _productService.UpdateAsync(productDto);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+
+            var wwwroot = _environment.WebRootPath;
+            var imagePath = Path.Combine(wwwroot, $"images\\{productDto.Image}");
+            ViewBag.ImageExist = System.IO.File.Exists(imagePath);
+
+            return View(productDto);
+        }
+
+        [HttpGet, ActionName("Delete")]
+        public async Task<IActionResult> DeleteView(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.RemoveAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
