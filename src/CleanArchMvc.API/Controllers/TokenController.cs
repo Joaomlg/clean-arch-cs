34633554cb204b034fb0e657bb9f1cbd431271d0
joaomlg <joaomarcos1999jm@gmail.com>
2021-12-04 10:31:41 -0300

    Configure authentication in controllers

--- src/CleanArchMvc.API/Controllers/TokenController.cs
+++ src/CleanArchMvc.API/Controllers/TokenController.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchMvc.API.Models;
 using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -13,6 +14,7 @@ namespace CleanArchMvc.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous]
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authentication;
